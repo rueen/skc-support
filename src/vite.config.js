764b5a55b6bfa8cf64e1896e5b116d89a53fbb94
diaochan <diaochan@seatent.com>
2025-03-02 22:08:58 +0800

    部署

--- src/vite.config.js
+++ src/vite.config.js
@@ -2,19 +2,22 @@
  * @Author: diaochan
  * @Date: 2025-03-02 21:49:03
  * @LastEditors: diaochan
- * @LastEditTime: 2025-03-02 21:58:52
+ * @LastEditTime: 2025-03-02 22:08:18
  * @Description: 
  */
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import path from 'path'
 
-export default defineConfig({
-  base: process.env.NODE_ENV === 'production' ? '/skc-support/' : '/',  // 替换 <repository-name> 为你的仓库名
+// 获取环境变量
+const env = process.env.NODE_ENV
+
+export default defineConfig(({ command, mode }) => ({
+  base: mode === 'production' ? '/skc-support/' : '/',
   plugins: [vue()],
   resolve: {
     alias: {
       '@': path.resolve(__dirname, 'src')
     }
   }
-}) 
+})) 
