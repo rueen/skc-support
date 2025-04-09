403f9722efcccfde161b021fc2f1c6aa9d9061fb
diaochan <diaochan@seatent.com>
2025-04-09 10:37:57 +0800

    多语言 channel

--- src/i18n/zh/channel.js
+++ src/i18n/zh/channel.js
@@ -2,7 +2,28 @@
  * @Author: diaochan
  * @Date: 2025-04-09 10:27:10
  * @LastEditors: diaochan
- * @LastEditTime: 2025-04-09 10:28:01
+ * @LastEditTime: 2025-04-09 10:37:23
  * @Description: 
  */
-export default {}
+export default {
+  add: '新增',
+  channelIcon: '渠道图标',
+  channelName: '渠道名称',
+  customFields: '自定义字段',
+  updateTime: '更新时间',
+  action: '操作',
+  edit: '编辑',
+  delete: '删除',
+  deleteConfirm: '确定要删除该渠道吗？',
+
+  addTitle: '新增渠道',
+  editTitle: '编辑渠道',
+
+  fansCount: '粉丝数',
+  friendsCount: '好友数',
+  postsCount: '发帖数',
+
+  channelNamePlaceholder: '请输入渠道名称',
+  iconRequired: '请上传渠道图标',
+  nameRequired: '请输入渠道名称',
+}
