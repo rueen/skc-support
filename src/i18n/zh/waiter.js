82a123097e61223beb0cab534054de1c93f6c95b
diaochan <diaochan@seatent.com>
2025-04-09 11:34:20 +0800

    多语言 channel

--- src/i18n/zh/waiter.js
+++ src/i18n/zh/waiter.js
@@ -2,7 +2,25 @@
  * @Author: diaochan
  * @Date: 2025-04-09 11:28:06
  * @LastEditors: diaochan
- * @LastEditTime: 2025-04-09 11:28:11
+ * @LastEditTime: 2025-04-09 11:34:12
  * @Description: 
  */
-export default {}
+export default {
+  add: '新增',
+  username: '用户名',
+  remarks: '备注',
+  action: '操作',
+  edit: '编辑',
+  delete: '删除',
+  deleteConfirm: '确定要删除该账号吗？',
+
+  addTitle: '新增账号',
+  editTitle: '编辑账号',
+  usernamePlaceholder: '请输入用户名',
+  usernameRequired: '请输入用户名',
+  password: '密码',
+  passwordPlaceholder: '请输入密码',
+  remarksPlaceholder: '请输入备注',
+  passwordRequired: '请输入密码',
+  permissions: '权限',
+}
