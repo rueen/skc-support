af15ade3e594c8469c29cd804422ca6864a54edc
diaochan <diaochan@seatent.com>
2025-04-10 14:54:26 +0800

    生产环境 API 基础路径

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-03-08 20:25:10
  * @LastEditors: diaochan
- * @LastEditTime: 2025-03-24 21:08:13
+ * @LastEditTime: 2025-04-10 14:54:13
  * @Description: 环境配置文件
  */
 
@@ -16,7 +16,7 @@ const API_BASE = {
   // 开发环境 API 基础路径
   development: 'http://localhost:3002',
   // 生产环境 API 基础路径
-  production: 'https://api.example.com',
+  production: 'http://api.rueen.cn',
 };
 
 // 公共 API 路径配置（用于图片上传、获取枚举常量等）
