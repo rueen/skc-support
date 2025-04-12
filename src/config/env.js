4699620d1e4b55be99fa016182a5c567fd3150d9
diaochan <diaochan@seatent.com>
2025-04-12 15:13:47 +0800

    接口域名

--- src/config/env.js
+++ src/config/env.js
@@ -2,7 +2,7 @@
  * @Author: diaochan
  * @Date: 2025-03-08 20:25:10
  * @LastEditors: diaochan
- * @LastEditTime: 2025-04-12 14:03:32
+ * @LastEditTime: 2025-04-12 15:12:25
  * @Description: 环境配置文件
  */
 
@@ -14,7 +14,7 @@ const isDev = process.env.NODE_ENV === 'development';
 // API 基础路径配置
 const API_BASE = {
   // 开发环境 API 基础路径
-  development: '',
+  development: '', // http://localhost:3002
   // 生产环境 API 基础路径
   production: 'http://sapi.rueen.cn',
 };
