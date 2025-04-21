5adf6d42b1ccfd2b79a99a35f2c4a78f3f45f28b
rueen <rueenli@gmail.com>
2025-04-21 07:04:48 +0000

    Deploying to gh-pages from @ rueen/skc-support@4df711a4ce021c85a2358621120d3e5b983c5626 🚀

--- assets/GroupOwner-CEf1NXlN.js
+++
@@ -1,1 +0,0 @@
-import{_ as e,g as a,p as r,o as n,w as s,i as c,t as p}from"./index-CgSCA0Xz.js";const _={};function i(o,l,m,u,f,d){const t=a("a-tag");return n(),r(t,{color:"blue"},{default:s(()=>[c(p(o.$t("common.groupOwner")),1)]),_:1})}const $=e(_,[["render",i]]);export{$ as G};
